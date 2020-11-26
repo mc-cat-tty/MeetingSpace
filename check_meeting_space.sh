da39f85c9e09e84edbedbac42182c50574739413
Francesco Mecatti <44820563+Francesco-Mecatti@users.noreply.github.com>
2020-11-26 11:50:52 +0100

    Add files via upload

---
+++ check_meeting_space.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Launch this script to check if given room_code is valid or not
+# Notice that meeting rooms are global and any valid authorization parameters (regardless of account) are suitable
+# Usage: ./meetIsActive.sh SAPISIDHASH x-goog-api-key x-goog-authuser __Secure-3PSID __Secure-3PAPISID room_code
+# Output: POST request HTTP status code
+#   200 -> meeting space exists
+#   404 -> meeting space doesn't exist
+#   400 -> bad request, the code is not valid
+
+# Parameter             Variable name
+# SAPISIDHASH           sapisidhash
+# x-goog-api-key        x_goog_api_key
+# x-goog-authuser       x_goog_authuser
+# __Secure-3PSID        secure_3psid
+# __Secure-3PAPISID     secure_3papisid
+# room_code             room_code
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]
+then
+    echo "Usage: ./meetIsActive.sh SAPISIDHASH x-goog-api-key x-goog-authuser __Secure-3PSID __Secure-3PAPISID room_code"
+    exit 0
+fi
+
+if (( "$#" != 6 ))
+then
+    echo "Illegal number of parameters"
+    echo "See usage with -h option"
+    exit 1
+fi
+
+sapisidhash="$1"
+x_goog_api_key="$2"
+x_goog_authuser="$3"
+secure_3psid="$4"
+secure_3papisid="$5"
+room_code="$6"
+
+curl 'https://meet.google.com/$rpc/google.rtc.meetings.v1.MeetingSpaceService/ResolveMeetingSpace' -H "authorization: SAPISIDHASH $sapisidhash" -H 'content-type: application/x-protobuf' -H "x-goog-api-key: $x_goog_api_key" -H "x-goog-authuser: $x_goog_authuser" -H 'x-goog-encode-response-if-executable: base64' -H 'Origin: https://meet.google.com' -H "Cookie: __Secure-3PSID=$secure_3psid; __Secure-3PAPISID=$secure_3papisid;" --data-raw "$(echo -en "\n\x0c"$room_code"0\x01")" -s -i \
+| head -n 1 \
+| grep -oP "HTTP/.+?\s\K\d+";
