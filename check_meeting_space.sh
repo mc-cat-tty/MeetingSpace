f7007bc8b2371b08d05f301b4e6d8bee29ebd2a0
Francesco Mecatti <44820563+Francesco-Mecatti@users.noreply.github.com>
2020-11-26 11:58:15 +0100

    Add files via upload

--- check_meeting_space.sh
+++ check_meeting_space.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Launch this script to check if given room_code is valid or not
+# Launch this script to check if given room_code is valid or not (whether it exists or not)
 # Notice that meeting rooms are global and any valid authorization parameters (regardless of account) are suitable
-# Usage: ./meetIsActive.sh SAPISIDHASH x-goog-api-key x-goog-authuser __Secure-3PSID __Secure-3PAPISID room_code
-# Output: POST request HTTP status code
+# Usage: ./check_meeting_space.sh SAPISIDHASH x-goog-api-key x-goog-authuser __Secure-3PSID __Secure-3PAPISID room_code
+# Output: POST response HTTP status code
 #   200 -> meeting space exists
 #   404 -> meeting space doesn't exist
 #   400 -> bad request, the code is not valid
@@ -18,7 +18,7 @@
 
 if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]
 then
-    echo "Usage: ./meetIsActive.sh SAPISIDHASH x-goog-api-key x-goog-authuser __Secure-3PSID __Secure-3PAPISID room_code"
+    echo "Usage: ./check_meeting_space.sh SAPISIDHASH x-goog-api-key x-goog-authuser __Secure-3PSID __Secure-3PAPISID room_code"
     exit 0
 fi
 
